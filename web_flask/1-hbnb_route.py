c7189e9b984647b04df21fc9f386fb2b18f05035
K-kare <kamunawangari@gmail.com>
2024-05-17 04:15:05 -0700

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,15 +6,11 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def home():
-    """
-    returns Hello HBNB!
-    """
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """
-    returns HBNB
-    """
+    """returns HBNB"""
     return 'HBNB'
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
